e465adabcdab4c441f96ade004b0a5f17665d609
samoula007 <62022815+samoula007@users.noreply.github.com>
2022-01-23 07:10:18 -0500

    Cleaning

--- g_ext/getPic.js
+++ g_ext/getPic.js
@@ -8,34 +8,8 @@ let images = [
 
 ]
 
-//let profilePics = document.getElementsByClassName("css-1dbjc4n r-yfoy6g r-1wyvozj r-633pao r-u8s1d r-1v2oles r-desppf");
-
-let website = document.getElementsByClassName("css-1dbjc4n")
-let pfp = website.item(0).getElementsByClassName("css-1dbjc4n r-yfoy6g r-1wyvozj r-633pao r-u8s1d r-1v2oles r-desppf")
-
-// website item 140?
-//console.log(pfp.length);
-//console.log(website.length);
-for (element of website) {
-    console.log(element);
-}
-
-// for (img of timeline) {
-//     console.log(img);
-// }
+let timeline = document.getElementsByClassName("css-1dbjc4n")
+//the profile pics on the timeline, doesn't work yet, but it's there
+let pfp = timeline.item(0).getElementsByClassName("css-1dbjc4n r-yfoy6g r-1wyvozj r-633pao r-u8s1d r-1v2oles r-desppf")
 
 
-
-
-// for (img of whatever) {
-//     let r = Math.floor(Math.random() * images.length)
-//     let image = images[r];
-//     let url = chrome.extension.getURL(image);
-//     img.src = url;
-//     console.log(url);
-
-// }
-
-
-
-//the timeline classname: "css-1dbjc4n"
